62093a08bbb5faa097dfc69e7a0c8fe4f6cce888
Emixham <Maximed.ipod@gmail.com>
2020-04-10 17:23:08 +0200

    petite modif

--- src/main/java/pizza/spring/controleur/AccueilControleur.java
+++ src/main/java/pizza/spring/controleur/AccueilControleur.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccueilControleur {
-	
+	//petite modif
 	@GetMapping({"/", "/accueil"})
 	public String accueil() {
 		return "accueil";
